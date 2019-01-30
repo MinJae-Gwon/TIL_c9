38eb0d10c57740ce1b5d4d5464f341ddc01246c8
MinJae_Gwon <minjae2271@gmail.com>
2019-01-30 03:23:00 +0000

    D21 |190130 AM | url name

---
+++ django/exercise2/students/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class  Student(models.Model):
+    name = models.TextField()
+    age = models.TextField()
+    email = models.TextField()
+    
