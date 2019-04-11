b5cc9b66dea9e7bfb6d419fb02ef80288dfae194
MinJae_Gwon <minjae2271@gmail.com>
2019-04-11 08:47:17 +0000

    190411 | Insta - Comment, Like, People

--- django/insta/posts/views.py
+++ django/insta/posts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import PostForm
-from .models import Post
+from django.views.decorators.http import require_POST, require_http_methods
+from .forms import PostForm, CommentForm
+from .models import Post, Comment
 
 
 def list(request):
     posts = Post.objects.order_by('-id').all()
-    return render(request, 'posts/list.html', {'posts': posts})
+    comment_form = CommentForm()
+    return render(request, 'posts/list.html', {'posts': posts, 'comment_form': comment_form})
 
 
 @login_required
@@ -25,7 +27,9 @@ def create(request):
         post_form = PostForm()
     
     return render(request, 'posts/form.html', {'post_form': post_form})
-    
+
+
+@login_required
 def update(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     
@@ -41,6 +45,7 @@ def update(request, post_id):
         post_form = PostForm(instance=post)
     return render(request, 'posts/form.html',{'post_form': post_form})
 
+@login_required
 #삭제
 # variable routing => post_id
 def delete(request, post_id):
@@ -53,5 +58,39 @@ def delete(request, post_id):
         return redirect('posts:list')
         
     post.delete()
+    
+    return redirect('posts:list')
+    
+
+@login_required
+@require_POST
+def comment_create(request, post_id):
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.user = request.user
+        comment.post_id = post_id
+        comment.save()
+    return redirect('posts:list')
+    
+
+@require_http_methods(['GET','POST'])
+def comment_delete(request, post_id, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if comment.user != request.user:
+        return redirect('posts:list')
+    
+    comment.delete()
+    return redirect('posts:list')
+    
+@login_required
+def like(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.user in post.like_users.all():
+        # 2. 좋아요 취소
+        post.like_users.remove(request.user)
+    else:
+        # 1. 좋아요!
+        post.like_users.add(request.user)
     
     return redirect('posts:list')
