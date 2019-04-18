30e660ee934976a6cb07d4c1612b18d802745cd5
MinJae_Gwon <minjae2271@gmail.com>
2019-04-18 07:44:07 +0000

    190418 | ORM - M:N

--- django/orm/onetomany/models.py
+++ django/orm/onetomany/models.py
@@ -15,12 +15,48 @@ class Comment(models.Model):
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+# 예시
+# 1. 1번 사람이 작성한 게시글은?
+# user1.post_set.all()
+
+# 2. 1번 사람이 작성한 게시글의 댓글들을 출력!
+# for post in user1.post_set.all():
+# ...     for comment in post.comment_set.all():
+# ...             print(comment.content)
+
+
+# 3. 2번(c2) 댓글을 쓴 사람은?
+# c2.user
+# <User: User object (2)>
+
+# 3-1. 2번 댓글을 쓴 사람이 쓴 모든 글은?
+# c2.user.post_set.all()
+
+# 4. 1번 글의 첫번 째 댓글을 쓴 사람의 이름은?
+# post1.comment_set.first().user.name #== post1.comment_set.all()[0].user.name
+# 'Kim'
+
+# 5. '1글'이 제목인 게시글은?
+# Post.objects.filter(title='1글')
+
+# 7. 댓글 중에 해당 게시글의 제목이 1글인 것은?
+# 방법1 
+# Comment.objects.filter(post_title='1글')
+# 방법2
+# Comment.objects.filter(post=post1)
+
+# 8. 댓글 중에 해당 게시글의 제목에 '1'이 들어가 있는 것은?
+# Comment.objects.filter(post__title__contains='1')
+
+
+
     
 # user1 = User.objects.create(name='Kim')
 # user2 = User.objects.create(name='Lee')
 
 # post1 = Post.objects.create(title='1글', user=user1) 
-# post2 = Post.objects.create(title='2글', user=user2)
+# post2 = Post.objects.create(title='2글', user=user1)
 # post3 = Post.objects.create(title='3글', user=user2)
 
 
